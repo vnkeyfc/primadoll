2c6e9509034dc135f17439237bc3ae22acfbd4ae
Cap Cap <rain.unmei@gmail.com>
2022-11-12 22:24:01 +0700

    Move dependency libs to venv folder

--- .tools/html2txt.bat
+++ .tools/html2txt.bat
@@ -1,15 +1,15 @@
 @echo off
 
-if exist ./py/Lib/ (
+if exist ./py/venv/Lib/ (
 	goto skip_install
 )
 
 echo Install require package...
-py -2 -m pip install --prefix "./py" -r "./py/requirements.txt"
+py -2 -m pip install --prefix "./py/venv" -r "./py/requirements.txt"
 if %ERRORLEVEL% NEQ 0 goto exit
 
 :skip_install
-set PYTHONPATH=./py/Lib/site-packages
+set PYTHONPATH=./py/venv/Lib/site-packages
 
 echo Extract text...
 py -2 "./py/main.py" -h2t "../.contents/original" "./txt_dump" -f 1
--- .tools/update_txt_trans.bat
+++ .tools/update_txt_trans.bat
@@ -1,15 +1,15 @@
 @echo off
 
-if exist ./py/Lib/ (
+if exist ./py/venv/Lib/ (
 	goto skip_install
 )
 
 echo Install require package...
-py -2 -m pip install --prefix "./py" -r "./py/requirements.txt"
+py -2 -m pip install --prefix "./py/venv" -r "./py/requirements.txt"
 if %ERRORLEVEL% NEQ 0 goto exit
 
 :skip_install
-set PYTHONPATH=./py/Lib/site-packages
+set PYTHONPATH=./py/venv/Lib/site-packages
 
 echo Update translate data...
 py -2 "./py/main.py" -t2h "../.translate" "../.translate/html" "./html_update" -f 1
