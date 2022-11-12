5dc994cfcdc00ce5aa7df5a2df777c48860c135c
Cap Cap <rain.unmei@gmail.com>
2022-11-12 21:58:12 +0700

    Move batch files to .tools folder

---
+++ .tools/html2txt.bat
@@ -0,0 +1,18 @@
+@echo off
+
+if exist ./py/Lib/ (
+	goto skip_install
+)
+
+echo Install require package...
+py -2 -m pip install --prefix "./py" -r "./py/requirements.txt"
+if %ERRORLEVEL% NEQ 0 goto exit
+
+:skip_install
+set PYTHONPATH=./py/Lib/site-packages
+
+echo Extract text...
+py -2 "./py/main.py" -h2t "../.contents/original" "./txt_dump" -f 1
+
+:exit
+pause
---
+++ .tools/update_xlsx_trans.bat
@@ -0,0 +1,18 @@
+@echo off
+
+if exist ./py/Lib/ (
+	goto skip_install
+)
+
+echo Install require package...
+py -2 -m pip install --prefix "./py" -r "./py/requirements.txt"
+if %ERRORLEVEL% NEQ 0 goto exit
+
+:skip_install
+set PYTHONPATH=./py/Lib/site-packages
+
+echo Update translate data...
+py -2 "./py/main.py" -t2h "../.translate/xlsx" "../.translate/html" "./html_update" -f 2
+
+:exit
+pause
