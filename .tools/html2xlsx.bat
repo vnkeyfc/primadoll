659bd729e31fead7467e153b058aaddedb374b37
Cạp Cạp <88388435+cap-cap@users.noreply.github.com>
2023-02-26 01:11:36 +0700

    bat: Migrate to python 3

--- .tools/html2xlsx.bat
+++ .tools/html2xlsx.bat
@@ -5,14 +5,14 @@ if exist ./py/venv/Lib/ (
 )
 
 echo Install require package...
-py -2 -m pip install --prefix "./py/venv" -r "./py/requirements.txt"
+py -3 -m pip install --prefix "./py/venv" -r "./py/requirements.txt"
 if %ERRORLEVEL% NEQ 0 goto exit
 
 :skip_install
 set PYTHONPATH=./py/venv/Lib/site-packages
 
 echo Extract text...
-py -2 "./py/main.py" -h2t "../.contents/original" "./xlsx_dump" -f 2
+py -3 "./py/main.py" -h2t "../.contents/original" "./xlsx_dump" -f 2
 
 :exit
 pause
